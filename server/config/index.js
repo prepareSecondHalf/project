a4f4dcb6a36e283dca1cacc56dd7e5540817e403
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2023-03-31 00:41:53 +0900

    [server] config server environment

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+console.log('DB_HOST:', process.env.MONGO_URI)
+console.log('DB_HOST:', process.env.PORT)
+
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT,
