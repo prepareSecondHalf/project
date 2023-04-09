7846ac3931a36995baae7114368110d03e435427
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2023-04-09 11:24:36 +0900

    task2, Header/Footer

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,11 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          type="text/javascript"
+          src="https://code.jquery.com/jquery-1.12.4.min.js"
+        />
+        <Script
+          type="text/javascript"
+          src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"
+        />
+      </Head>
       <body>
         <Header />
         <Main />
