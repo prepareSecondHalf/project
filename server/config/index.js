345b31ce33952e6b634d7efcd49b9568cc0d1603
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2023-05-06 22:07:10 +0900

    task4

--- server/config/index.js
+++ server/config/index.js
@@ -2,10 +2,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-console.log('DB_HOST:', process.env.MONGO_URI)
-console.log('DB_HOST:', process.env.PORT)
+console.log('DB_HOST:', process.env.MONGO_URI);
+console.log('DB_HOST:', process.env.PORT);
 
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
