8944cf5d77c91f442667530a89a2065e179a387b
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2023-06-03 15:04:11 +0900

    task3

--- client/pages/chat.tsx
+++ client/pages/chat.tsx
@@ -1,195 +1,20 @@
 import { useInput } from 'hooks/useInput';
 import { NextPage } from 'next';
 import { Key, useEffect, useState } from 'react';
-import axios from 'axios';
-
 import io from 'socket.io-client';
 
-interface IMessage {
-    user: string;
-    message: string;
-}
-
-const dummyChats = [
-    {
-        expert: '정환',
-        msg: [
-            {
-                id: 0,
-                isMe: false,
-                name: '정환',
-                msg: '혹시 디자인 파일이 있으실까요?? 디자인 파일 없이 웹사이트 제작은 힘듭니다 ㅠㅠ',
-            },
-            {
-                id: 1,
-                isMe: true,
-                msg: '앗 아뇨... 디자인 파일이 따로 필요한거였군요 ㅠㅠㅠ... 혹시 아시는 분이 있으실까요??',
-            },
-            {
-                id: 2,
-                isMe: true,
-                msg: '제가 처음 맡겨보는거라 몰랐네요 ㅠㅠ',
-            },
-            {
-                id: 3,
-                isMe: false,
-                name: '정환',
-                msg: '음... 010-2797-7760 여기로 연락해 보시겠어요?? 디자이너는 아닌데 어깨가 깡패입니다',
-            },
-            {
-                id: 4,
-                isMe: true,
-                msg: '엇... 그게 무슨 상관인가요 디자인이랑...? 근데 어깨가 깡패라니 좀 설레네요... 혹시 그 분 나이가...',
-            },
-            {
-                id: 5,
-                isMe: false,
-                name: '정환',
-                msg: '?? 소개팅 자리가 아닌데요',
-            },
-            {
-                id: 6,
-                isMe: true,
-                msg: '?? 아니 그... 그럼 디자인이랑 뭔상관이신 분인가요?',
-            },
-            {
-                id: 7,
-                isMe: false,
-                name: '정환',
-                msg: 'ㅋㅋ 뭔소린지 이게',
-            },
-        ],
-    },
-    {
-        expert: '희수',
-        msg: [
-            {
-                id: 0,
-                isMe: false,
-                name: '희수',
-                msg: '혹시 디자인 파일이 있으실까요?? 디자인 파일 없이 웹사이트 제작은 힘듭니다 ㅠㅠ',
-            },
-            {
-                id: 1,
-                isMe: true,
-                msg: '앗 아뇨... 디자인 파일이 따로 필요한거였군요 ㅠㅠㅠ... 혹시 아시는 분이 있으실까요??',
-            },
-            {
-                id: 2,
-                isMe: true,
-                msg: '제가 처음 맡겨보는거라 몰랐네요 ㅠㅠ',
-            },
-            {
-                id: 3,
-                isMe: false,
-                name: '희수',
-                msg: '음... 010-2797-7760 여기로 연락해 보시겠어요?? 디자이너는 아닌데 어깨가 깡패입니다',
-            },
-            {
-                id: 4,
-                isMe: true,
-                msg: '엇... 그게 무슨 상관인가요 디자인이랑...? 근데 어깨가 깡패라니 좀 설레네요... 혹시 그 분 나이가...',
-            },
-            {
-                id: 5,
-                isMe: false,
-                name: '희수',
-                msg: '?? 소개팅 자리가 아닌데요',
-            },
-            {
-                id: 6,
-                isMe: true,
-                msg: '?? 아니 그... 그럼 디자인이랑 뭔상관이신 분인가요?',
-            },
-            {
-                id: 7,
-                isMe: false,
-                name: '희수',
-                msg: 'ㅋㅋ 뭔소린지 이게',
-            },
-        ],
-    },
-    {
-        expert: '승규',
-        msg: [
-            {
-                id: 0,
-                isMe: false,
-                name: '승규',
-                msg: '혹시 디자인 파일이 있으실까요?? 디자인 파일 없이 웹사이트 제작은 힘듭니다 ㅠㅠ',
-            },
-            {
-                id: 1,
-                isMe: true,
-                msg: '앗 아뇨... 디자인 파일이 따로 필요한거였군요 ㅠㅠㅠ... 혹시 아시는 분이 있으실까요??',
-            },
-            {
-                id: 2,
-                isMe: true,
-                msg: '제가 처음 맡겨보는거라 몰랐네요 ㅠㅠ',
-            },
-            {
-                id: 3,
-                isMe: false,
-                name: '승규',
-                msg: '음... 010-2797-7760 여기로 연락해 보시겠어요?? 디자이너는 아닌데 어깨가 깡패입니다',
-            },
-            {
-                id: 4,
-                isMe: true,
-                msg: '엇... 그게 무슨 상관인가요 디자인이랑...? 근데 어깨가 깡패라니 좀 설레네요... 혹시 그 분 나이가...',
-            },
-            {
-                id: 5,
-                isMe: false,
-                name: '승규',
-                msg: '?? 소개팅 자리가 아닌데요',
-            },
-            {
-                id: 6,
-                isMe: true,
-                msg: '?? 아니 그... 그럼 디자인이랑 뭔상관이신 분인가요?',
-            },
-            {
-                id: 7,
-                isMe: false,
-                name: '승규',
-                msg: 'ㅋㅋ 뭔소린지 이게',
-            },
-        ],
-    },
-];
-
-const dummyChatters = [
-    {
-        id: 0,
-        name: '정환',
-        latestChat:
-            '혹시 디자인 파일이 있으실까요?? 디자인 파일 없이 웹사이트 제작은 힘듭니다 ㅠㅠ',
-    },
-    {
-        id: 1,
-        name: '희수',
-        latestChat:
-            '혹시 디자인 파일이 있으실까요?? 디자인 파일 없이 웹사이트 제작은 힘듭니다 ㅠㅠ',
-    },
-    {
-        id: 2,
-        name: '승규',
-        latestChat:
-            '혹시 디자인 파일이 있으실까요?? 디자인 파일 없이 웹사이트 제작은 힘듭니다 ㅠㅠ',
-    },
-];
-
 interface IChat {
+    _id: Key;
     roomId: String;
     user: String;
     message: String;
     createdAt: Date;
+    creator: String;
 }
 interface IRoom {
     roomId: String;
     owner: String;
+    ownername: String;
     chats: IChat[];
 }
 
@@ -231,9 +56,7 @@ const Chat: NextPage = () => {
             opponentUserId: user,
         });
 
-        socketClient.on('createChat', (res) => {
-            console.log('createChatRes:::', res);
-        });
+        socketClient.on('createChat', (res) => {});
     };
 
     const sendMsg = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -243,15 +66,23 @@ const Chat: NextPage = () => {
             path: '/api/chat',
         });
 
-        socketClient.emit('sendMsg', { roomId, userId: user, msg: msg.value });
+        socketClient.emit('sendMsg', {
+            roomId,
+            userId: user,
+            msg: msg.value,
+            username: '윤제혁',
+        });
 
         socketClient.on('sendMsg', (res) => {
             console.log('sendMsgRes:::', res);
         });
+
+        msg.resetValue();
     };
 
     const handleChats = (roomId: String) => {
         setChats(rooms.filter((item) => item.roomId === roomId)[0].chats);
+
         setRoomId(roomId);
     };
 
@@ -259,28 +90,31 @@ const Chat: NextPage = () => {
         <div className="w-full min-w-[1200px] flex justify-center">
             <div className="w-[1170px] h-fit my-0 mx-auto mt-32 flex border border-[#e1e1e4]">
                 <div className="flex-1 h-[500px] overflow-y-auto bg-[#F5F6FA] relative">
-                    {dummyChats
-                        .filter((item) => item.expert === '정환')[0]
-                        .msg.map((item) => {
-                            return (
-                                <div key={item.id} className="w-full h-fit p-4">
-                                    {item.isMe ? (
-                                        <div className="w-full flex justify-end">
-                                            <div className="w-fit max-w-sm h-fit bg-white rounded-lg p-3 border-[#e1e1e4] border">
-                                                {item.msg}
-                                            </div>
+                    {chats.map((item) => {
+                        return (
+                            <div
+                                key={item._id}
+                                className={`w-full h-fit p-4 ${
+                                    item.user === user ? 'pt-0' : ''
+                                }`}
+                            >
+                                {item.user === user ? (
+                                    <div className="w-full flex justify-end">
+                                        <div className="w-fit max-w-sm h-fit bg-white rounded-lg p-3 border-[#e1e1e4] border">
+                                            {item.message}
                                         </div>
-                                    ) : (
-                                        <div>
-                                            <div>{item.name}</div>
-                                            <div className="w-fit max-w-sm h-fit bg-white rounded-lg p-3 border-[#e1e1e4] border">
-                                                {item.msg}
-                                            </div>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <div>{item.creator}</div>
+                                        <div className="w-fit max-w-sm h-fit bg-white rounded-lg p-3 border-[#e1e1e4] border">
+                                            {item.message}
                                         </div>
-                                    )}
-                                </div>
-                            );
-                        })}
+                                    </div>
+                                )}
+                            </div>
+                        );
+                    })}
                     <div className="sticky bottom-0 left-0 bg-white w-full h-12 border-t border-[#e1e1e4] flex justify-center flex-col px-2">
                         <form
                             className="w-full h-8 flex gap-2"
@@ -289,7 +123,8 @@ const Chat: NextPage = () => {
                             <input
                                 className="flex-1 h-full border border-[#e1e1e4] rounded-md px-3 focus:border-[#e1e1e4] focus:outline-none"
                                 placeholder="메세지를 입력해주세요."
-                                {...msg}
+                                value={msg.value}
+                                onChange={msg.onChange}
                             />
                             <button className="w-fit h-full bg-basered text-white rounded-md px-4">
                                 Send
@@ -307,7 +142,10 @@ const Chat: NextPage = () => {
                             >
                                 <div className="flex-1">
                                     <div className="font-semibold">
-                                        {item.owner}
+                                        {item.ownername}{' '}
+                                        <span className="font-normal text-gray-500 text-sm">
+                                            {item.chats[0].message}
+                                        </span>
                                     </div>
                                 </div>
                             </div>
--- server/models/chat.js
+++ server/models/chat.js
@@ -2,25 +2,26 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const {
-  Types: { ObjectId },
+    Types: { ObjectId },
 } = Schema;
 
 const ChatSchema = new Schema({
-  roomId: { type: ObjectId, required: true, ref: 'chatRoom' },
-  user: {
-    type: ObjectId,
-    required: true,
-    ref: 'user',
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
+    roomId: { type: String, required: true },
+    user: {
+        type: ObjectId,
+        required: true,
+        ref: 'user',
+    },
+    creator: { type: String, required: true },
+    message: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
 });
 
 const Chat = mongoose.model('chat', ChatSchema);
