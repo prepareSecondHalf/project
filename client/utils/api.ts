353d0f73117c5cda0220bc9cc93e0547bf0b3dcc
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2023-07-24 19:45:16 +0900

    PUBLIC_SERVER_URL

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: `${process.env.PUBLIC_SERVER_URL}/api`,
   headers: {
     'Content-Type': 'application/json; charset=UTF-8',
     accept: 'application/json',
